91695b4160cc00361b0403fe428e019cb24321e3
DejanIkic <dikic19999@gmail.com>
2024-04-21 14:03:03 +0200

    dodan dio prijavljenog korisnika u navigaciji

--- CKReact/ck_react/src/styles/colors.js
+++ CKReact/ck_react/src/styles/colors.js
@@ -3,8 +3,10 @@ const theme = {
   colors: {
     primary: "#007bff",
     secondary: "#6c757d",
+    nav_bg: "#E5E7EB",
     nav_btn_active: "#cBcAcA",
     nav_btn: "#FAFAFA",
+    nav_user_bg: "#D5D7DB",
     // Add more colors as needed
   },
 };
