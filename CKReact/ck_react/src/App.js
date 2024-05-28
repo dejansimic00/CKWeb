9edbee4b77da445141b04c065f492e5d5eab1b17
Pavle Colic <pavle99colic@gmail.com>
2024-05-28 14:13:36 +0200

    Change to App.js

--- CKReact/ck_react/src/App.js
+++ CKReact/ck_react/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import NavBar from "./components/navigation/NavBar/NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import Dashboard from "./pages/common/Dashboard";
 import Login from "./pages/common/Login";
@@ -9,12 +8,42 @@ import Residents from "./pages/user/Residents";
 import Settings from "./pages/common/Settings";
 import { AuthContext } from "./context/AuthContext";
 import { useAuth } from "./hooks/useAuth";
-import { useState, useEffect } from "react";
-import theme from "./styles/colors";
 import AdminNavBar from "./components/navigation/NavBar/AdminNavBar";
 import Volunteer from "./pages/admin/Volunteer";
 import Notification from "./pages/admin/Notification";
 import Camp from "./pages/admin/Camp";
+import Place from "./pages/admin/Place";
+import {useEffect} from "react";
+
+function MainLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log("Current path:", location.pathname);
+  }, [location.pathname]);
+
+  return (
+    <div className="flex">
+      {/* Conditionally render AdminNavBar based on the current path */}
+      {location.pathname !== "/login" && <AdminNavBar />}
+
+      <div className="w-full">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/residents" element={<Residents />} />
+          <Route path="/camp" element={<Camp />} />
+          <Route path="/volunteer" element={<Volunteer />} />
+          <Route path="/notification" element={<Notification />} />
+          <Route path="/place" element={<Place />} />
+        </Routes>
+      </div>
+      <div className="bg-red-100 w-96"></div>
+    </div>
+  );
+}
 
 function App() {
   const { user, setUser } = useAuth();
@@ -22,23 +51,7 @@ function App() {
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <Router>
-        <div className="flex">
-          <AdminNavBar />
-
-          <div className="w-full">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/archive" element={<Archive />} />
-              <Route path="/residents" element={<Residents />} />
-              <Route path="/camp" element={<Camp />} />
-              <Route path="/volunteer" element={<Volunteer />} />
-              <Route path="/notification" element={<Notification />} />
-            </Routes>
-          </div>
-          <div className="bg-red-100 w-96"></div>
-        </div>
+        <MainLayout />
       </Router>
     </AuthContext.Provider>
   );
